cf817a66cd21813bc7059a46ab2071fed04db9bb
johnsona9 <adamjohnson35@gmail.com>
2015-08-10 21:22:06 -0400

    viewing postDetails works

--- uSell/PostDetailsViewController.swift
+++ uSell/PostDetailsViewController.swift
@@ -11,10 +11,18 @@ import Parse
 
 class PostDetailsViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var editionLabel: UILabel!
+    @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
     var post:PFObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.titleLabel.text = self.post["postTitle"] as? String
+        self.editionLabel.text = self.post["postEdition"] as? String
+        self.classLabel.text = self.post["postClass"] as? String
+        self.costLabel.text = self.post["postCost"] as? String
         // Do any additional setup after loading the view.
     }
 
